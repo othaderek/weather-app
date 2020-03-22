8a6d017f47768436f7cc009e6f4716403e6d83c9
othaderek <otha.hernandez@gmail.com>
2020-03-22 16:03:49 -0400

    added new endpoint.

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const request = require('request');
 const url = 'https://api.darksky.net/forecast/1d67ceb0ef3226c2b9f0f7630b90794a/40.635810,%20-73.945136';
+const mapKey = 'pk.eyJ1Ijoib2RocCIsImEiOiJjazgzNDEwbGwwNmc1M21vZ2w1N3lqbzZqIn0.FXLYCAbblCg8037MujnQsA';
+const mapEndpoint = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoib2RocCIsImEiOiJjazgzNDEwbGwwNmc1M21vZ2w1N3lqbzZqIn0.FXLYCAbblCg8037MujnQsA'
 
-request({ url: url}, async (e, res) => {
+// request({ url: url}, async (e, res) => {
+//     const data = await JSON.parse(res.body)
+//     forecast(data);
+// })
+request({ url: mapEndpoint}, async (e, res) => {
     const data = await JSON.parse(res.body)
-    forecast(data);
+    let latitude = data.features[0].center[1]
+    let longitude = data.features[0].center[0]
+
+    console.log(latitude, longitude);
+    
 })
 
 const forecast = (data) => {
